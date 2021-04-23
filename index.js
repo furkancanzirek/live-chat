1fdf77aec81eafe506786be71a9766692788997e
furkancanzirek <can.zirek@ogr.sakarya.edu.tr>
2021-04-23 15:43:33 +0300

    port güncellemesi
    port güncellemesi

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-const express= require('express')
+const express = require('express')
 const socket = require('socket.io')
-PORT =process.env.PORT ||8080
+PORT = process.env.PORT || 5000
 const app = express()
 const server = app.listen(PORT)
 
 app.use(express.static("public"))
 
 
-    const io=socket(server)
-    io.on('connection',(socket)=>{
-        console.log(socket.id);
-        socket.on('chat',data=>{
-            io.sockets.emit('chat',data)
-        })
-        socket.on('typing',data=>{
-            socket.broadcast.emit('typing',data)
-        })
+const io = socket(server)
+io.on('connection', (socket) => {
+    console.log(socket.id);
+    socket.on('chat', data => {
+        io.sockets.emit('chat', data)
     })
-
-
+    socket.on('typing', data => {
+        socket.broadcast.emit('typing', data)
+    })
+})

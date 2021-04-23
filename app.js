9f86cef903528a90882c6f37a51a519dda60bd25
furkancanzirek <can.zirek@ogr.sakarya.edu.tr>
2021-04-23 15:16:36 +0300

    app.js
    app.js güncellemesi

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const server = app.listen(8080)
 
 app.use(express.static("public"))
 const io=socket(server)
-io.on('connection',(socket)=>{
-    console.log(socket.id);
-    socket.on('chat',data=>{
-        io.sockets.emit('chat',data)
+app.get('/',(req, res)=>{
+    io.on('connection',(socket)=>{
+        console.log(socket.id);
+        socket.on('chat',data=>{
+            io.sockets.emit('chat',data)
+        })
+        socket.on('typing',data=>{
+            socket.broadcast.emit('typing',data)
+        })
     })
-    socket.on('typing',data=>{
-        socket.broadcast.emit('typing',data)
-    })
+    
 })

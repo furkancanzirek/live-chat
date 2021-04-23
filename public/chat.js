68cca09dc43552d96abe6b613f247a33b5a09249
furkancanzirek <can.zirek@ogr.sakarya.edu.tr>
2021-04-23 15:49:12 +0300

    socket güncellemesi
    socket güncellemesi

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-const socket=io.connect('http://localhost:8080')
+const socket=io.connect('https://live-chat-app-furkan.herokuapp.com')
 
 const sender=document.getElementById('sender')
 const submitBtn=document.getElementById('submitBtn')
